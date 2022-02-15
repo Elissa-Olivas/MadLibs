857a17ce71b487ec6d9cec609f30052e757b0a80
Elissa Olivas <elissaolivas@Elissas-MacBook-Pro.local>
2022-02-15 13:52:31 -0700

    Added Story

---
+++ src/com/company/MadLibs.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	String[] noun= {"Teacher", "Trampoline", "Phoenix", "Phone", "Museum"};
+    }
+    String[] adjective= {"new", "blue", "sparkly", "tall", "wealthy"};
+    String[] verb= {"resist", "bounce", "fell", "hike", "sing"};
+
+
+
+}
